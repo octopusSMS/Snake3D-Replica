6f38598d5f47d2ca9c8414f6a59d068e3d56708c
octopusSMS <boronovskiy.evgeniy@gmail.com>
2022-11-01 22:52:00 +0300

    Хвост следует за головой, но тормозит.

--- Assets/Scripts/SnakeScript.cs
+++ Assets/Scripts/SnakeScript.cs
@@ -5,7 +5,7 @@ public class SnakeScript : MonoBehaviour
 {
     public bool IsHead = false;
 
-    public Transform previouspartposition;
+    public GameObject previouspart;
 
     [Min(0)]
     public float Speed;
@@ -13,6 +13,8 @@ public class SnakeScript : MonoBehaviour
     [SerializeField] private LayerMask layerMask;
 
     public Material[] SnakeMaterial;
+
+    public Vector3 _previousposition;
     private void UpdateMaterial() //метод для изменения цвета частей змеи
     {
         GetComponent<Renderer>().sharedMaterial = IsHead ? SnakeMaterial[0] : SnakeMaterial[1];
@@ -30,7 +32,10 @@ public class SnakeScript : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButton(0)) // при нажатии левой кнопки мыши
+        _previousposition = transform.position; //сохраняем положение
+
+
+        if (Input.GetMouseButton(0)&& IsHead) // при нажатии левой кнопки мыши
         {
             Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition); //из камеры вылетает луч 
             if (Physics.Raycast(Ray, out RaycastHit rayCastHit, float.MaxValue, layerMask)) //определяется точка пересечения луча и объекта с layer=flore
@@ -38,8 +43,14 @@ public class SnakeScript : MonoBehaviour
             
         }
 
-        previouspartposition = transform;
-        transform.position += new Vector3(0, 0, Speed * Time.deltaTime);
+        if (IsHead)
+            transform.position += new Vector3(0, 0, Speed * Time.deltaTime);
+        else
+        {
+            Vector3 delta = transform.position - previouspart.GetComponent<SnakeScript>()._previousposition; 
+            transform.position -= delta*Speed*2*Time.deltaTime;
+        }
+        
                 
     }
 }
