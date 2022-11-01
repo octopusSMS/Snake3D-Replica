2573d2075e0fdf52d88f7d14009b8770d3a39685
octopusSMS <boronovskiy.evgeniy@gmail.com>
2022-11-01 22:24:13 +0300

    Создан скрипт перемещения змеи за курсором

---
+++ Assets/Scripts/SnakeScript.cs
@@ -0,0 +1,36 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SnakeScript : MonoBehaviour
+{
+    public bool IsHead = false;
+
+    public Transform previouspartposition;
+
+    public Material[] SnakeMaterial;
+
+    [SerializeField] private LayerMask layerMask;
+    private void UpdateMaterial() //метод для изменения цвета частей змеи
+    {
+        GetComponent<Renderer>().sharedMaterial = IsHead ? SnakeMaterial[0] : SnakeMaterial[1];
+    }
+
+    private void OnValidate() //метод Unity для внесения изменений прямо в окне редактора, без запуска игры
+    {
+        UpdateMaterial();
+    }
+
+    private void Awake()
+    {
+        UpdateMaterial();
+    }
+
+    private void Update()
+    {
+        if (!Input.GetMouseButton(0)) return; // при нажатии левой кнопки мыши
+
+        Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition); //из камеры вылетает луч 
+        if (Physics.Raycast(Ray, out RaycastHit rayCastHit, float.MaxValue, layerMask)) //определяется точка пересечения луча и объекта с layer=flore
+            transform.position = new Vector3(rayCastHit.point.x, 0.5f, transform.position.z); //позиция передается объекту
+    }
+}
