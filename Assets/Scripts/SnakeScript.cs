fb72d06267699b7aa179f4714d94ca68a644835c
octopusSMS <boronovskiy.evgeniy@gmail.com>
2022-11-01 22:36:55 +0300

    Змея научилась ползать вперед

--- Assets/Scripts/SnakeScript.cs
+++ Assets/Scripts/SnakeScript.cs
@@ -7,9 +7,12 @@ public class SnakeScript : MonoBehaviour
 
     public Transform previouspartposition;
 
-    public Material[] SnakeMaterial;
+    [Min(0)]
+    public float Speed;
 
     [SerializeField] private LayerMask layerMask;
+
+    public Material[] SnakeMaterial;
     private void UpdateMaterial() //метод для изменения цвета частей змеи
     {
         GetComponent<Renderer>().sharedMaterial = IsHead ? SnakeMaterial[0] : SnakeMaterial[1];
@@ -27,10 +30,16 @@ public class SnakeScript : MonoBehaviour
 
     private void Update()
     {
-        if (!Input.GetMouseButton(0)) return; // при нажатии левой кнопки мыши
+        if (Input.GetMouseButton(0)) // при нажатии левой кнопки мыши
+        {
+            Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition); //из камеры вылетает луч 
+            if (Physics.Raycast(Ray, out RaycastHit rayCastHit, float.MaxValue, layerMask)) //определяется точка пересечения луча и объекта с layer=flore
+                transform.position = new Vector3(rayCastHit.point.x, 0.5f, transform.position.z); //позиция передается объекту
+            
+        }
 
-        Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition); //из камеры вылетает луч 
-        if (Physics.Raycast(Ray, out RaycastHit rayCastHit, float.MaxValue, layerMask)) //определяется точка пересечения луча и объекта с layer=flore
-            transform.position = new Vector3(rayCastHit.point.x, 0.5f, transform.position.z); //позиция передается объекту
+        previouspartposition = transform;
+        transform.position += new Vector3(0, 0, Speed * Time.deltaTime);
+                
     }
 }
