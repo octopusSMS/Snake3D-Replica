728d0bc588bfe29d5cb07a23dbe647c526068898
octopusSMS <boronovskiy.evgeniy@gmail.com>
2022-11-09 23:38:59 +0300

    Подготовка к реализации постоянства в игре

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -3,8 +3,9 @@ using Random = System.Random;
 
 public class BlockScript : MonoBehaviour
 {
-    
+    [Min(1)]
     public int Difficulty;
+    public Material[] Material;
     private GameControllerScript Game;
     private TextMesh Text;
 
@@ -15,6 +16,26 @@ public class BlockScript : MonoBehaviour
         Difficulty = random.Next(1, 5);
         Text = GetComponentInChildren<TextMesh>();
         Text.text = Difficulty.ToString();
+
+        switch (Difficulty)
+        {
+            case 1:
+                GetComponent<Renderer>().sharedMaterial = Material[0];
+                break;
+            case 2:
+                GetComponent<Renderer>().sharedMaterial = Material[1];
+                break;
+            case 3:
+                GetComponent<Renderer>().sharedMaterial = Material[2];
+                break;
+            case 4:
+                GetComponent<Renderer>().sharedMaterial = Material[3];
+                break;
+            case 5:
+                GetComponent<Renderer>().sharedMaterial = Material[4];
+                break;
+        }
+        //GetComponent<Renderer>().sharedMaterial
     }
 
     private void OnTriggerEnter(Collider other)
